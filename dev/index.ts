14c57b83ae13b669b8bbdfbd24bc1b68e8c40004
Jake Boone <jakeboone02@gmail.com>
2024-06-08 14:27:41 -0700

    Move common dev code out of main package directory

--- dev/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './App';
-export * from './DevLayout';
-export * from './constants';
-export * from './musicalInstruments';
-export * from './types';
-export * from './useDevApp';
-export * from './utils';
